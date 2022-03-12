3c645c4e9b6c5a6d2a9cee1979d7285299be29a8
root <inezapatience2@gmail.com>
2022-03-12 05:39:29 -0800

    Displaying a phrase using puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+*main- prints the phrase "Programmng is like builiding a
+*multilingual puzzle"
+*Return: 0 if exited properly,no n-zero otherwise
+*/
+int main(void)
+{puts("\"Programming is like builiding a multilingual puzz")
+return (0);
+}
