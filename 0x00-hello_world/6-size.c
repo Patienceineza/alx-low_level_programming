b79b17f25399576be39fb0b80cfb94a606b15350
patienceineza <inezapatience2@gmail.com>
2022-03-14 00:09:36 -0700

    Printing size of datatypes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,10 +4,10 @@
 *Return: 0 if ended well, otherwise nonzero
 */
 int main(void)
-{printf("size of a char: % byte(s)\n", sizeof(char));
-printf("size of an int: % byte(s)\n", sizeof(int));
-printf("size of a long int: % byte(S)\n", sizeof(long int));
-printf("size of long long int: % byte(S)\n", sizeof(long long int));
-printf("size of a float % byte(S)\n", sizeof(float));
+{printf("size of a char: %d byte(s)\n", sizeof(char));
+printf("size of an int: %d byte(s)\n", sizeof(int));
+printf("size of a long int: %d byte(S)\n", sizeof(long int));
+printf("size of long long int: %d byte(S)\n", sizeof(long long int));
+printf("size of a float %d byte(S)\n", sizeof(float));
 return (0);
 }
