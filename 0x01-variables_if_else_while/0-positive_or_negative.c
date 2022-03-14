be6dee09e7f5d636dd4af3db1fd1c66460c663b7
patienceineza <inezapatience2@gmail.com>
2022-03-14 02:23:10 -0700

    Showing if intered numer is positive or negative

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+*mai - get random number and print the number
+*check if it is positive or negative
+*Return 0 if correctly ended
+*/
+int main(void)
+{
+int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+if (n > 0)
+{
+printf("%d is negative\n", n);
+}
+else if (n == 0)
+{
+printf("%d is zero\n", n);
+}
+else
+{
+printf("%d is negative\n", n);
+return (0);
+}
