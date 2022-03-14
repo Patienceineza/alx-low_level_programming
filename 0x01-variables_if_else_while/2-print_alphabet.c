e571d0c501727e4be59c717999d7d765c861b13a
patienceineza <inezapatience2@gmail.com>
2022-03-14 05:37:01 -0700

    Printing alphabet except q and e

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+*main- prints using putchar,printing 
+*all letters except q and e
+*Return: 0
+*/
+int main(void)
+{
+char c = 'a'
+while (c <= 'z')
+{
+
+if (c != 'q' && c != 'e')
+putchar(C);
+c++;
+}
+putchar('\n');
+return (0);
+}
