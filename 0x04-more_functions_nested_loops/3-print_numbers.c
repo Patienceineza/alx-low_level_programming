8e337c9171b42cc214ea68957aeb9db94d3c3639
patienceineza <inezapatience2@gmail.com>
2022-03-18 02:47:53 -0700

    Printing numbers 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* print_numbers - function that prints thr numbers
+* @void: no return
+*/
+
+void print_numbers(void)
+{
+char i;
+
+for (i - '0'; i <= '9'; i++)
+{
+_putchar(i);
+}
+_putchar('\n');
+}
