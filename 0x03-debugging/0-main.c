823206b0dc99723293e0fa46b858396caf303397
patienceineza <inezapatience2@gmail.com>
2022-03-17 23:24:04 -0700

    My commit message

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,13 +1,15 @@
 #include "main.h"
 /**
-*main - tests function thet prints if intiger
-*is positive or negative 
-*Return: 0
+* main - main
+* Return:0
 */
 int main(void)
 {
 int i;
 i = 0;
-positive _or_negative(i);
+
+positive_0r_negative(i);
+
 return (0);
+
 }
