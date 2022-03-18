d10910f8294df732e6e699ceeb772f3eba74da0f
patienceineza <inezapatience2@gmail.com>
2022-03-18 01:16:02 -0700

    Remaining days of year

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -11,6 +11,7 @@
 */
 
 void print_remaining_days(int month, int day, int year)
+{
 if ((year % 4 == 0 && !(year % 100 == 0)) || year % 400 == 0)
 {
 if (month > 2 && day >= 60)
@@ -20,11 +21,15 @@ day ++;
 printf("Day of the year: %d\n", day);
 printf("Reamaining dats: %d\n", 366 - day);
 }
-else{
+else
+{
 if (month  == 2 && day ==60)
 {
 printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
 }
+else  
+{
+printf("Day of year: %d\n", day);	
 printf("Remeining days: %d\n", 365 - day);
 }
 }
