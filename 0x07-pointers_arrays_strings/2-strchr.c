4e5e0c99bef1564c7a2d25558949bf022f0c92c6
patienceineza <inezapatience2@gmail.com>
2022-03-28 06:11:39 -0700

    Third commit

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+*__strchr - locates a character in a string
+*@s: string
+*@c: character to search
+*Return: pointer to the firt occurence of the charact
+*er c in the string s
+*/
+char *_strchr(char *s, char c)
+{
+while (*s != '\0')
+/* Declring while */	
+{  
+if (*s == c)
+{
+return (s);
+}
+++s;
+}
+if (*s == c)
+{
+return (s);
+}
+return (0);
+}
