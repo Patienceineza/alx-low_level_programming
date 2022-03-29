adeed09fb29115d281ab29f81426c1365fa23c52
patienceineza <inezapatience2@gmail.com>
2022-03-29 03:53:38 -0700

    Copy string

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+*set_string - sets the value to a pointer to achar
+*@s:content to be copied
+*@to: string
+*Return: void
+*/
+void set_string(char **s, char *to)
+{
+*s = to;
+}
