73ae060b0048804e66a28c3be4786b18f32cd5e8
patienceineza <inezapatience2@gmail.com>
2022-03-29 02:21:30 -0700

    Fifth commit

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+* _strpbrk - matches any characters specified
+* @s: this is the c string to e scaneed
+* @accept:  cgaracter in str1 thet matches charcter in str2
+*Return: string s thet matches any character specified in accept
+*/
+char *_strpbrk(char *s, char *accept)
+{
+int j;
+while (*s != '\0')
+{
+j = 0;
+while (accept[j] != '\0')
+{
+if (*s == accept[j])
+{
+return (s);
+}
+j++;
+}
+s++
+}
+return (0);
+}
